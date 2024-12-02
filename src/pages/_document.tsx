506d482644cec9399168514a4fe7d6b37cd93ac5
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2024-12-02 19:27:03 -0300

    adjusting heading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Dooing - A minimalist todo list manager for Neovim"
+        />
+        <meta name="theme-color" content="#16161E" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Dooing - Todo Manager for Neovim</title>
+      </Head>
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
